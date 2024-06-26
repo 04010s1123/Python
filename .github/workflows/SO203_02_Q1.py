9f3c89b267395fbbf22c9aa663603f68c3e83061
04010s1123 <ichigo_ichie_h@outlook.jp>
2024-06-27 01:34:59 +0900

    Add files via upload

---
+++ .github/workflows/SO203_02_Q1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+プログラミング基礎Python
+基本構文
+練習問題1
+「これはPythonの例文です。」を10回表示してください。
+
+静的解析ツール
+================
+>>> pycodestyle SO203_02_Q1.py
+================
+
+実行例
+================
+>>> python SO203_02_Q1.py
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+これはPythonの例文です。
+================
+
+'''
+
+
+# ここより下に解答を記載する。
+print('>>> python SO203_02_Q1.py')
+for i in range(10):
+  print('これはPythonの例文です。')
